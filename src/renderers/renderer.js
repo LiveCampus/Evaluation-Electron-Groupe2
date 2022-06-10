a6bb5890875270ec2ed1363face15d2f9099d7ac
Théo Posty <theo.posty@livecampus.tech>
2022-06-10 16:47:23 +0200

    card styles

--- src/renderers/renderer.js
+++ src/renderers/renderer.js
@@ -5,17 +5,40 @@ window.addEventListener("DOMContentLoaded", async () => {
   window.api.receive("async:list:read", (data) => {
     for (let i = 0; i < data.length; i++) {
       const newList = document.createElement("div");
+      const newListTitle = document.createElement("div");
+      const newListContent = document.createElement("div");
+
       newList.classList.add(
         "bg-slate-200",
+        "rounded-lg",
+        "flex",
+        "flex-col",
+        "w-full",
+        "self-start",
+        "min-h-[100px]"
+      );
+      newListTitle.classList.add(
+        "uppercase",
+        "bg-slate-300",
         "py-1",
         "px-3",
-        "rounded-sm",
-        "flex-auto",
-        "uppercase"
+        "rounded-t-lg"
       );
-      newList.id = `list-${data[i].id}`;
-      newList.innerText = `${data[i].title} (${data[i].count})`;
+      newListContent.classList.add(
+        "px-4",
+        "py-2",
+        "flex",
+        "flex-col",
+        "items-center",
+        "justify-center",
+        "grow"
+      );
 
+      newListContent.id = `list-${data[i].id}`;
+      newListTitle.innerText = `${data[i].title} (${data[i].count})`;
+      newListContent.innerHTML = "<div class='no-tasks'>No Tasks</div>";
+
+      newList.append(newListTitle, newListContent);
       lists.append(newList);
     }
     window.api.send("task:read");
@@ -24,10 +47,25 @@ window.addEventListener("DOMContentLoaded", async () => {
   window.api.receive("async:task:read", (data) => {
     for (let i = 0; i < data.length; i++) {
       const newCard = document.createElement("div");
+
+      newCard.classList.add("bg-white", "p-4", "w-full", "my-1");
+
       newCard.innerText = `${data[i].title}`;
 
       const list = document.getElementById(`list-${data[i].idList}`);
+
+      Array.from(list.children).map((child) => {
+        if (child.classList.contains("no-tasks")) {
+          child.remove();
+        }
+      });
+
       list.append(newCard);
     }
+
+    /*for (let j = 0; j < list.children.length; j++) {
+      if (list.children[i] && list.children[i].classList.contains("no-tasks"))
+        list.children[i].remove();
+    }*/
   });
 });
