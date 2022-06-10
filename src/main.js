f864adac43f1f526e9dd0ce32c5c7acd95066241
Théo Posty <theo.posty@livecampus.tech>
2022-06-10 11:58:57 +0200

    add boilerplate

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preloads/preload.js"),
+    },
+  });
+
+  win.loadFile("src/views/index.html");
+};
+
+app.on("ready", () => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
